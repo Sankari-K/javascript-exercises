d0603f3d76864de130ef20f93a0ccdcdde7a5847
Sankari Karthik <sankarikarthik03@gmail.com>
2022-06-30 11:17:20 +0530

    Add solutions for first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
 const removeFromArray = function() {
+    array = arguments[0];
+    for (let i = 1; i < arguments.length; i++)
+    {
+        let indexOfElem = findIndex(array, arguments[i]);
+        if (indexOfElem !== false)
+        {
+            array.splice(indexOfElem, 1);
+        }
+    }
+    console.log(array);
+    return array;
+};
 
+function findIndex(array, elem) {
+    for (let i = 0; i < array.length; i++)
+    {
+        if (elem === array[i])
+        {
+            return i;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    let sum = 0;
+    if (typeof(start) != "number" || typeof(stop) != "number") {
+        return "ERROR";
+    }
+    if (start < 0 || stop < 0) {
+        return "ERROR";
+    }
+    let temp = start;
+    start = start < stop ? start: stop;
+    stop = stop >= temp ? stop: temp;
+    console.log("test", start, stop);
+    for(let i = start; i <= stop; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
